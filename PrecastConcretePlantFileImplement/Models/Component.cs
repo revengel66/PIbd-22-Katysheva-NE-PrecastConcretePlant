d16a913334c3f10648c65c8836e37abc66b91b00
revengel <natalia200218@yandex.ru>
2022-03-02 22:34:28 +0400

    lab2

---
+++ PrecastConcretePlantFileImplement/Models/Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecastConcretePlantFileImplement.Models
+{
+    /// <summary>
+    /// Компонент, требуемый для изготовления изделия
+    /// </summary>
+    public class Component
+    {
+        public int Id { get; set; }
+        public string ComponentName { get; set; }
+    }
+
+}
---
+++ PrecastConcretePlantFileImplement/Models/Reinforced.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecastConcretePlantFileImplement.Models
+{
+    /// <summary>
+    /// Изделие, изготавливаемое в магазине
+    /// </summary>
+    public class Reinforced
+    {
+        public int Id { get; set; }
+        public string ReinforcedName { get; set; }
+        public decimal Price { get; set; }
+        public Dictionary<int, int> ReinforcedComponents { get; set; }
+
+    }
+}
